8e7030891edcec99a76989d5f565698d0668954a
Dan <rodger.swan@gmail.com>
2020-10-19 20:52:02 +1100

    app setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is the beginning of the weSkool app.
+    </div>
+  );
+}
+
+export default App;
