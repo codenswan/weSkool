7ab618e427a213542b71f3ee11de89ed92599ef6
Dan <rodger.swan@gmail.com>
2020-10-19 20:51:24 +1100

    server setup

--- server.js
+++ server.js
@@ -1,24 +1,48 @@
+const express = require('express');
+const logger = require('morgan');
+const compression = require('compression');
+const mongoose = require('mongoose');
+const path = require('path');
 
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+//* middleware
 const app = express();
-
 app.use([
-    express.urlencoded({ extended: true }),
-    express.json()
+  express.urlencoded({ extended: true }),
+  express.json(),
+  compression(),
+  logger('dev'),
 ]);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
-// Link API Routes here
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//* mongoose config
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
+//* mongo setup
+const mongodb = mongoose.connection;
+// eslint-disable-next-line no-console
+mongodb.on('error', console.error.bind(console, 'connection error:'));
+// eslint-disable-next-line no-console
+mongodb.once('open', () => console.log('Connected to database.'));
+
+// Link API Routes here
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+//* port config
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("🚀  Server server now on port", PORT, "👻 React App on Port 3000");
+  console.log(
+    '🚀  Server server now on port',
+    PORT,
+    '👻 React App on Port 3000'
+  );
 });
