daffa41821d5eb3d555333dcd8e6af8cff473cfc
Dan <rodger.swan@gmail.com>
2020-10-22 17:53:36 +1100

    routes setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const compression = require('compression');
 const mongoose = require('mongoose');
-const path = require('path');
+const routes = require('./routes');
 
 //* middleware
 const app = express();
@@ -16,6 +16,10 @@ app.use([
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+//* Link API Routes here
+app.use(routes);
+
 
 //* mongoose config
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weSkool', {
@@ -31,11 +35,6 @@ const mongodb = mongoose.connection;
 mongodb.on('error', console.error.bind(console, 'connection error:'));
 // eslint-disable-next-line no-console
 mongodb.once('open', () => console.log('Connected to database.'));
-
-// Link API Routes here
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
 //* port config
 const PORT = process.env.PORT || 3001;
