8564e4b1e2706d0e1014679c28283cfd5e8361f2
Dan <rodger.swan@gmail.com>
2020-10-24 16:41:42 +1100

    activity form setup

--- client/src/pages/Activities.jsx
+++ client/src/pages/Activities.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form } from 'react-bootstrap';
 
 const Activities = () => {
-    return (
-        
-      <Container className="pageContainer"> 
-          <div>This is were all the activities will be displayed sorted by data</div>
-          <input type="file"></input>
+  return (
+    <>
+      <Container className="pageContainer">
+        <Form>
+          <Form.Group>
+            <Form.Label>Activity</Form.Label>
+            <Form.Control name="activityName" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Description</Form.Label>
+            <Form.Control name="description" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label
+              className="my-1 mr-2"
+              htmlFor="inlineFormCustomSelectPref"
+            >
+              Subject
+            </Form.Label>
+            <Form.Control
+              as="select"
+              className="my-1 mr-sm-2"
+              id="inlineFormCustomSelectPref"
+              custom
+            >
+              <option value="0">Choose...</option>
+              <option value="1">English</option>
+              <option value="2">Maths</option>
+              <option value="3">Science</option>
+              <option value="4">HSIE</option>
+              <option value="5">Creative Arts</option>
+              <option value="6">PDHPE</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Form.File id="custom-file" label="Upload doc or photo" />
+          </Form.Group>
+        </Form>
       </Container>
+    </>
   );
 };
 
