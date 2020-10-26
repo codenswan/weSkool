00101e8cfa45ab91497a5a8a1ff5d8761d68447e
Dan <rodger.swan@gmail.com>
2020-10-26 13:15:31 +1100

    student button component works

--- client/src/components/StudentButtons/StudentButtons.jsx
+++ client/src/components/StudentButtons/StudentButtons.jsx
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom';
 import { Button, ButtonGroup } from 'react-bootstrap';
 
 const StudentButtons = (props) => {
-    console.log(props);
-  return (
+    return (
     <ButtonGroup vertical>
       {props.studentRoll.map((student) => (
-        <Link to={`/students/log/${props.student._id}`} key={props.student._id}>
-          <Button id="studentBtns">{props.student.name}</Button>
+        <Link to={`/students/log/${student._id}`} key={student._id}>
+          <Button id="studentBtns">{student.name}</Button>
         </Link>
       ))}
     </ButtonGroup>
